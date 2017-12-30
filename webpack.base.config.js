e9098dcaad7162e09b0b4e6fd84ba26433a1d278
jkyle <jkyle@causethought.com>
2017-12-30 14:48:12 -0600

    Bump packages.

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -10,7 +10,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       options: {
-        presets: ['es2015', 'react'],
+        presets: ['env', 'react'],
         plugins: ['transform-object-rest-spread']
       }
     },
